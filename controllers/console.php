88c8fbff460bfecee0f9500e48f5d2627998470e
Tomas Sardyha <darsain@gmail.com>
2012-11-16 13:14:40 +0100

    Cleanup

--- controllers/console.php
+++ controllers/console.php
@@ -12,25 +12,24 @@ class Console_Console_Controller extends Controller {
 		Asset::container('footer')->bundle('console');
 
 		/* Styles */
-		Asset::container('header')->add('normalize', 'css/normalize.css');
-		Asset::container('header')->add('main', 'css/main.css', 'normalize');
-		Asset::container('header')->add('codemirror', 'css/codemirror/codemirror.css');
-		Asset::container('header')->add('codemirror-theme', 'css/codemirror/theme/'.Config::get('console.theme', Config::get('console::console.theme')).'.css', 'codemirror');
+		Asset::container('header')
+			->add('normalize', 'css/normalize.css')
+			->add('main', 'css/main.css', 'normalize')
+			->add('codemirror', 'css/codemirror/codemirror.css')
+			->add('codemirror-theme', 'css/codemirror/theme/'.Config::get('console.theme', Config::get('console::console.theme')).'.css', 'codemirror');
 
 		/* Scripts */
 		Asset::container('header')->add('modernizr', 'js/vendor/modernizr.js');
 
-		Asset::container('footer')->add('jquery', 'js/vendor/jquery.min.js');
-		Asset::container('footer')->add('plugins', 'js/vendor/plugins.js', 'jquery');
+		Asset::container('footer')
+			->add('jquery', 'js/vendor/jquery.min.js')
+			->add('plugins', 'js/vendor/plugins.js', 'jquery')
 
-		Asset::container('footer')->add('codemirror', 'js/vendor/codemirror.js');
-		// Asset::container('footer')->add('codemirror-xml', 'js/vendor/mode/xml.js', 'codemirror');
-		// Asset::container('footer')->add('codemirror-javascript', 'js/vendor/mode/javascript.js', 'codemirror');
-		// Asset::container('footer')->add('codemirror-css', 'js/vendor/mode/css.js', 'codemirror');
-		Asset::container('footer')->add('codemirror-clike', 'js/vendor/mode/clike.js', 'codemirror');
-		Asset::container('footer')->add('codemirror-php', 'js/vendor/mode/php.js', 'codemirror');
+			->add('codemirror', 'js/vendor/codemirror.js')
+			->add('codemirror-clike', 'js/vendor/mode/clike.js', 'codemirror')
+			->add('codemirror-php', 'js/vendor/mode/php.js', 'codemirror')
 
-		Asset::container('footer')->add('main', 'js/main.js', array('jquery', 'plugins', 'codemirror'));
+			->add('main', 'js/main.js', array('jquery', 'plugins', 'codemirror'));
 
 		parent::__construct();
 	}
@@ -54,14 +53,14 @@ class Console_Console_Controller extends Controller {
 
 		// Execute and profile the time
 		ob_start();
-		$c8r3j4v9r3j_start = microtime(true); // "namespaced" so it won't get overriden
+		$start_c8r3j4v9r3j = microtime(true); // "namespaced" so it won't get overriden
 		eval($code);
 		$end = microtime(true);
 		$output = ob_get_clean();
 
 		// Response
 		return Response::json(array(
-			'time'        => number_format(($end - $c8r3j4v9r3j_start) * 1000, 2),
+			'time'        => number_format(($end - $start_c8r3j4v9r3j) * 1000, 2),
 			'time_total'  => number_format((microtime(true) - LARAVEL_START) * 1000, 2),
 			'memory'      => get_file_size(memory_get_usage(true)),
 			'memory_peak' => get_file_size(memory_get_peak_usage(true)),
