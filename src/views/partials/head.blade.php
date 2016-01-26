333a8203f675295b2855f6c4f53503a8a1bc0315
standaniels <mail@standaniels.nl>
2016-01-26 10:20:37 +0100

    Add _token meta tag

--- src/views/partials/head.blade.php
+++ src/views/partials/head.blade.php
@@ -3,6 +3,7 @@
 	<head>
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        	<meta name="_token" content="{{ Session::token() }}">
 		<title>Laravel Console</title>
 		<meta name="viewport" content="width=device-width">
 
